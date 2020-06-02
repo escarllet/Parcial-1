7ec350727bcb2447ee896af5c1e7b1d06006b9df
Escarllet Suriel Valdez <Escarlletsurielvaldez@hotmail.com>
2020-06-02 18:01:13 -0500

    Identifique si es fin de semana. version 1.0

---
+++ Dia de la semana/Dia de la semana/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia_de_la_semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string semana;
+            Console.WriteLine("Introduzca el nombre de un dia de la semana (Primera letra en mayus.)");
+            semana = Console.ReadLine();
+            if (semana == "Sabado" || semana == "Domingo")
+            {
+                Console.WriteLine("Es fin de semana");
+            }
+            else if (semana == "Lunes" || semana == "Martes" || semana == "Miercoles" || semana == "Jueves" || semana == "Viernes")
+            {
+                Console.WriteLine("No es fin de semana");
+            }
+            else 
+            {
+                Console.WriteLine("Error: El dia de la semana no existe o esta mal  escrito");
+            }
+            Console.ReadKey();
+        }
+    }
+}
